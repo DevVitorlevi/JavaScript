9c965d2aa496500cdca56a4b430a8fee039b1f23
VT69 <160429477+Vt69xx@users.noreply.github.com>
2024-07-06 18:04:57 -0300

    main

---
+++ REPETIÇÃO/fatorial.js
@@ -0,0 +1,7 @@
+let num =5
+let fat =1
+while(num>=1){
+    fat*=num
+    num--
+}
+console.log(fat)
---
+++ REPETIÇÃO/node_4a56e3d7aa13b.tmp.js
@@ -0,0 +1,9 @@
+let num= 5
+let res = 1
+let i = 1
+while(i>0){
+    res*=num
+    i++
+    
+}
+console.log(res)
