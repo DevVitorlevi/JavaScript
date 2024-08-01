094d22e5b617fcebdd2ba60d626edb86f2795d05
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-01 20:22:35 -0300

    alt

--- EVENTOS DOM/refatora.js
+++ EVENTOS DOM/refatora.js
@@ -1,61 +1,66 @@
-function fundo (){
-    let c= document.getElementById('caixa')
-    let f = document.getElementById('fundo')
-    let b1= document.getElementById('btn1')
-    let b2= document.getElementById('btn2')
-    c.addEventListener('click', () =>{
-        f.style.background='#4B0082'
-        f.style.transition='all ease-in-out 1s'
-        c.style.background='white'
-        c.innerHTML='Clicou!!'
-        c.style.color='#000000'
-        b1.style.background='#4B0082'
-        b2.style.background='#4B0082'
+function Bottom (){ 
+    let Box= document.getElementById('bx')
+    let Bottom = document.getElementById('bottom')
+    let ButtonBottom= document.getElementById('btn1')
+    let ButtonColor= document.getElementById('btn2')
+    Box.addEventListener('click', () => {
+        Bottom.style.background='#4B0082'
+        Bottom.style.transition='all ease-in-out 1s'
+        Box.style.background='#ffffff'
+        Box.innerHTML='Clicou!!'
+        Box.style.color='#000000'
+        ButtonBottom.style.background='#4B0082'
+        ButtonColor.style.background='#4B0082'
+        ButtonBottom.style.transition='all ease-in-out 1s'
+        ButtonColor.style.transition='all ease-in-out 1s'
     })
-    c.addEventListener('mouseenter',()=>{
-        f.style.background='#8B0000'
-        f.style.transition='all ease-in-out 1s'
-        c.style.background='white'
-        c.innerHTML='Entrou!!'
-        c.style.color='#000000'
-        b1.style.background='#8B0000'
-        b2.style.background='#8B0000'
-        b2.style.boxShadow='none'
-        b1.style.boxShadow='none'
+    Box.addEventListener('mouseenter',() => {
+        Bottom.style.background='#8B0000'
+        Bottom.style.transition='all ease-in-out 1s'
+        Box.style.background='#ffffff'
+        Box.innerHTML='Entrou!!'
+        Box.style.color='#000000'
+        ButtonBottom.style.background='#8B0000'
+        ButtonColor.style.background='#8B0000'
+        ButtonColor.style.boxShadow='none'
+        ButtonBottom.style.boxShadow='none'
+        ButtonBottom.style.transition='all ease-in-out 1s'
+        ButtonColor.style.transition='all ease-in-out 1s'
+        
     })
-    c.addEventListener('mouseout',()=>{
-        f.style.background='#191970'
-        f.style.transition='all ease-in-out 1s'
-        c.style.background='white'
-        c.innerHTML='Saiu!!'
-        c.style.color='#000000'
-        b1.style.background='#191970'
-        b2.style.background='#191970'
-        b1.style.transition='all ease-in-out 1s'
-        b2.style.transition='all ease-in-out 1s'
+    Box.addEventListener('mouseout',()=>{
+        Bottom.style.background='#191970'
+        Bottom.style.transition='all ease-in-out 1s'
+        Box.style.background='#FFFFFF'
+        Box.innerHTML='Saiu!!'
+        Box.style.color='#000000'
+        ButtonBottom.style.background='#191970'
+        ButtonColor.style.background='#191970'
+        ButtonBottom.style.transition='all ease-in-out 1s'
+        ButtonColor.style.transition='all ease-in-out 1s'
     })
 }
-function caixa(){
-    let box = document.getElementById('caixa')
+function Box(){
+    let box = document.getElementById('bx')
     box.addEventListener('click',()=>{
         box.style.transition='all ease-in-out 1s'
         box.style.background='#4B0082'
         box.innerHTML='Clicou!!'
-        box.style.color='white'
+        box.style.color='#ffffff'
         box.style.boxShadow= '10px 10px 325px #DA70D6'
     })
     box.addEventListener('mouseenter',()=>{
         box.style.transition='all ease-in-out 1s'
         box.style.background='#8B0000'
         box.innerHTML='Entrou!!'
-        box.style.color='white'
+        box.style.color='#ffffff'
         box.style.boxShadow= '10px 10px 125px #FA8072'
     })
     box.addEventListener('mouseout',()=>{
         box.style.transition='all ease-in-out 1s'
         box.style.background='#191970'
         box.innerHTML='Saiu!'
-        box.style.color='white'
+        box.style.color='#ffffff'
         box.style.boxShadow= '10px 10px 125px #3838ce'
     })
 }
