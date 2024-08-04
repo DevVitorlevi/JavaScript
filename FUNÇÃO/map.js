ba7f33985ac0b25cfd6603bb72297e1474f11847
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-03 21:55:47 -0300

    atl

---
+++ FUNÇÃO/map.js
@@ -0,0 +1,5 @@
+const c = ["HTML","CSS","JS","PHP","MYSQL"]
+c.map((el,p )=>{
+    console.log(`Cursos:${el}  - Posição:${p}`)
+})
+// o metodo map é usado para percorrer arrays e é possivel passar 2 param ex: map(el:este vai mostar os elementos,p: este a posição dos elementos)
---
+++ FUNÇÃO/returnmap.js
@@ -0,0 +1,6 @@
+const cursos = ["HTML","CSS","JAVA","PHP"]
+let c = cursos.map((el)=>{
+    return el
+})
+console.log(c)
+// fazendo o retorno dos elmentos dentrom do array
