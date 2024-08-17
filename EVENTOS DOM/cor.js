67c175db0eddf578ebd766b276e9679789243c94
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-17 19:43:48 -0300

    att

--- EVENTOS DOM/cor.js
+++
@@ -1,5 +0,0 @@
-function color (){
-    let Corinput = document.getElementById('color').value 
-    // variavel recebe o valor do input
-    document.body.style.background = Corinput
- } // Evento DOM body recebe o valor que foi inserido ao input no caso a variavel Corinput
