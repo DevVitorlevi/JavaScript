40b8720f06fe3aca6ae6f9c3e44eb9b6a63dcf3c
Dev Vitor <160429477+Dev-Vitor-lev@users.noreply.github.com>
2024-08-17 19:37:27 -0300

    att

---
+++ EVENTOS DOM/children.js
@@ -0,0 +1,6 @@
+const Box = document.getElementById('box1')
+const Cursos = [...document.querySelectorAll('.cursos')]
+
+console.log(Box.children)//Todos Os Elementos dentro da div Box 
+console.log(Box.firstElementChild)//O primeiro Elemento da div Box
+console.log(Box.lastElementChild)//O Ultimo Elemneto Da Div Box 
