2efabf7841e18de2d72f73d286ff308fbdf6de8a
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-08-20 08:42:13 -0300

    att

--- EVENTOS/Creaset.js
+++ EVENTOS/Creaset.js
@@ -1,1 +1,10 @@
-const box =document.querySelector('box')
+const Box =document.querySelector('.box')
+const Carros = ['Opala','UNO','Chevette','Palio','Corsa','Ford KA','Siena']
+
+Carros.map((el)=>{
+    const NovoCarro = document.createElement('div')
+    NovoCarro.setAttribute("class","curso")
+    NovoCarro.innerHTML =el
+    Box.appendChild(NovoCarro)
+})
+
