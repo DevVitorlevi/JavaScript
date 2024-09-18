45c8609331bd360a4faed25054a30f8cecc2ec07
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-09-17 21:46:29 -0300

    att

--- POO/car.js
+++
@@ -1,9 +0,0 @@
-class Car{
-    constructor(c){
-        this.color=c
-    }
-}
-var car = new Car("Azul")
-var car2 =new Car("Preto")
-console.log(car.color)
-console.log(car2.color)
--- POO/set.js
+++ POO/set.js
@@ -34,13 +34,4 @@ class Car{
         this.vel = velo
     }
 }//O metodo Set como ja diz sua tradução tem a capacidade de alterar a propiedade de um objeto antes dele ser acionado    
-var c1 = new Car ("Relâmpago Marquinhos",1)
-var c2 = new Car ("Pálio 2 Portas",2)
-var c3 = new Car ("BMW",3)
-var c4 = new Car ("Tanque",4)
 
-c1.setNome("UNO")
-c1.setTipo("Popular")
-c1.setVelo(500)
-// Nesta ocasião a alteração de propiedade acontece mudando os valores definidos anteriomente
-c1.info()
