3601e3a7fdde238e3e697a9727af26e309cde299
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-29 18:37:31 -0300

    arr

---
+++ ARRAY/foreach.js
@@ -0,0 +1,3 @@
+const aprovados = ['Bruna','Vitor','Felipe']
+
+aprovados.forEach((nome,i,arr)=>console.log(nome + i + arr))//o Foreach é uma maneitra de percorrer array, inforando elemneto o inice e o array
